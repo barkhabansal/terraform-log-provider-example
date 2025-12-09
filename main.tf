82d306109a906ce71a51e7bc34bccf10c9685478
Barkha Bansal <barkha.bansal@mastercard.com>
2025-12-09 21:12:08 +0530

    Fixed issues

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     log-provider = {
       source = "local/log-provider"
-      version = "0.0.1"
     }
   }
 }
@@ -10,6 +9,6 @@ terraform {
 provider "log-provider" {
 }
 
-resource "log-provider-log" "barkha-log" {
+resource "log-provider_log" "barkha-log" {
   message = "hello ME :)"
 }
